ac0fc78cb40fc30871bbf6e75f89ad18fae56bb2
qinran0423 <1156107187@qq.com>
2020-11-29 17:59:29 +0800

    composition api的简单使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,54 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <h2>{{msg2}}</h2>
+  <p>{{counter}}</p>
+  <p>{{doubleCounter}}</p>
+  <p ref="desc"></p>
 </template>
 
 <script>
+import { reactive, computed, onMounted,onUnmounted, ref, toRefs, watch } from 'vue'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
-  data() {
-    return {
-      count: 0
-    }
+  setup() {
+    const {counter, doubleCounter} = useCounter()
+
+    let msg2 = ref('message')
+
+    const desc = ref(null)
+
+    watch(counter, (val, oldVal) => {
+      const p = desc.value
+      p.textContent = `counter change from ${oldVal} to ${val}`
+    })
+
+    return {counter, doubleCounter, msg2, desc}
   }
 }
+
+
+function useCounter() {
+  const data = reactive({
+    counter: 1,
+    doubleCounter: computed(() => data.counter *2)
+  })
+
+  let timer
+
+  onMounted(() => {
+    timer = setInterval(() => {
+      data.counter ++
+    }, 1000)
+  })
+
+  onUnmounted(() => {
+    clearInterval(timer)
+  })
+
+  return toRefs(data)
+}
+
 </script>
