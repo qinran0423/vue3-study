f46400a69b780415d8528dbb3090534001df3900
qinran0423 <1156107187@qq.com>
2021-09-04 23:30:58 +0800

    探究reactive

---
+++ mini-vue3/test.js
@@ -0,0 +1,27 @@
+const { effect, reactive } = require('@vue/reactivity')
+
+// let a = 10;
+// let b;
+
+// function update() {
+//   b = a + 10;
+//   console.log(b);
+// }
+
+
+// a = 20;
+// update()
+
+
+let a = reactive({
+  value: 1
+});
+let b;
+effect(() => {
+  // 此函数会自动执行一次
+  b = a.value + 10
+  console.log(b);
+})
+
+// a的值发生了变化  effect中的函数将再次执行
+a.value = 30
