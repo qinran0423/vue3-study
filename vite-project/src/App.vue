5707371e6caf896c5b535a46eafae3000af82bff
qinran0423 <1156107187@qq.com>
2021-09-12 22:28:41 +0800

    add js-cookie

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-    <i class="el-icon-picture"></i>
+  <i class="el-icon-picture"></i>
 
-  <router-view/>
+  <router-view />
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style>
-
-</style>
+<style></style>
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -13,4 +13,13 @@ import './styles/index.scss'
 // element3
 import element3 from 'plugins/element3'
 
-createApp(App).use(router).use(store).use(element3).mount('#app')
+import Cookies from 'js-cookie'
+
+
+Cookies.set('foo', 'bar')
+Cookies.set('age', 12)
+
+
+Cookies.set('%2%', 1, { expires: 7, path: '' })
+console.log(Cookies.get('%2%'));
+createApp(App).use(router).use(store).use(element3).use(Cookies).mount('#app')
