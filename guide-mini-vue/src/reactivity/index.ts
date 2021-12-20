9dac157c47ff4354fc897370e4afff80a5095b86
qinran0423 <1156107187@qq.com>
2021-12-20 22:45:46 +0800

    复盘mini-vue

---
+++ guide-mini-vue/src/reactivity/index.ts
@@ -0,0 +1,4 @@
+// 出口
+export function add(a, b) {
+  return a + b
+}
---
+++ guide-mini-vue/src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,42 @@
+import { effect } from "../effect"
+import { reactive } from "../reactive"
+
+
+
+describe('effect', () => {
+  it('happy path', () => {
+    const user = reactive({
+      age: 10
+    })
+
+    let nextAge
+    effect(() => {
+      nextAge = user.age + 1
+      console.log(nextAge);
+
+    })
+
+    expect(nextAge).toBe(11)
+
+    // update
+    user.age++
+    expect(nextAge).toBe(12)
+
+  })
+
+
+  it('runner', () => {
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe('foo')
+
+  })
+})
