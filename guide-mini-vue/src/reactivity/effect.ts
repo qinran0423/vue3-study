0b3cc31f8958e2bb414bdd846e1d32320627e1b0
qinran0423 <1156107187@qq.com>
2021-12-21 17:09:59 +0800

    复盘Stop

--- guide-mini-vue/src/reactivity/effect.ts
+++ guide-mini-vue/src/reactivity/effect.ts
@@ -1,7 +1,11 @@
+import { extend } from "../../share"
 
 
 class ReactiveEffect {
   private _fn: any
+  deps = []
+  active = true
+  onStop?: () => void
   constructor(fn, public scheduler?) {
     this._fn = fn
   }
@@ -11,8 +15,25 @@ class ReactiveEffect {
     activeEffect = this
     return this._fn()
   }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
 }
 
+
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  });
+}
 
 
 const targetMap = new Map()
@@ -32,7 +53,7 @@ export function track(target, key) {
   }
 
   dep.add(activeEffect)
-
+  activeEffect.deps.push(dep)
 }
 
 
@@ -57,10 +78,19 @@ let activeEffect
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler)
 
+  extend(_effect, options)
 
   _effect.run()
 
-  return _effect.run.bind(_effect)
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner
+
+}
 
 
+
+export function stop(runner) {
+  runner.effect.stop()
 }
