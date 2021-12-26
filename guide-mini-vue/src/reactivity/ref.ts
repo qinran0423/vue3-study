ee13f9c14986cd6e11fa9a8cd2d8433f315c353f
qinran0423 <1156107187@qq.com>
2021-12-26 16:47:17 +0800

    复盘 isRef unRef

--- guide-mini-vue/src/reactivity/ref.ts
+++ guide-mini-vue/src/reactivity/ref.ts
@@ -7,6 +7,7 @@ class RefImpl {
   private _value: any;
   public dep
   private _rawValue: any;
+  public __v_isRef = true
   constructor(value) {
     this._rawValue = value
     this._value = convert(value)
@@ -43,4 +44,13 @@ function trackRefValue(ref) {
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+
+export function isRef(ref) {
+  return !!ref.__v_isRef
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref
 }
