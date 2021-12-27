c01cfa4fe029c2bb4a150d87dc365e574b95f415
qinran0423 <1156107187@qq.com>
2021-12-27 16:54:18 +0800

    复盘computed

---
+++ guide-mini-vue/src/reactivity/computed.ts
@@ -0,0 +1,42 @@
+import { ReactiveEffect } from "./effect";
+
+
+
+class ComputedRefTmpl {
+  private _getter: any;
+  private _value: any;
+  private _dirty: boolean = true;
+  private _effect: ReactiveEffect;
+
+  constructor(getter) {
+    this._getter = getter
+
+    this._effect = new ReactiveEffect(getter, () => {
+      if (!this._dirty) {
+        this._dirty = true
+      }
+    })
+
+
+  }
+
+
+
+
+
+
+  get value() {
+
+    if (this._dirty) {
+      this._dirty = false
+      this._value = this._effect.run()
+    }
+    return this._value
+  }
+
+}
+
+
+export function computed(getter) {
+  return new ComputedRefTmpl(getter)
+}
