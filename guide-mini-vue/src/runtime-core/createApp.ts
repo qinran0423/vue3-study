2daeca5d1bf9df702c7f63dd19fe97b5ecaaae11
qinran0423 <1156107187@qq.com>
2021-12-28 18:50:18 +0800

    复盘组件实例化

---
+++ guide-mini-vue/src/runtime-core/createApp.ts
@@ -0,0 +1,19 @@
+import { render } from "./renderer"
+import { createVNode } from "./vnode"
+
+
+
+export function createApp(rootComponent) {
+
+
+  return {
+    mount(rootContainer) {
+      const vnode = createVNode(rootComponent)
+
+      render(vnode, rootContainer)
+    }
+  }
+
+}
+
+
